44a87af6e59414672366083b384d78de61213786
gonka-1 <gdrc579@gmail.com>
2025-04-07 11:03:38 -0400

    Update PR.py

--- PR.py
+++ PR.py
@@ -3,3 +3,26 @@ Nombre= input ('Hola, dime tu nombre')
 Edad = int (input('Cuantos años tienes '))
 
 print ('Hola', Nombre, 'Tienes:', Edad)
+
+
+
+
+print('Hola, mundo')
+
+
+""""se utuliza para dejar comentarios
+de varias lineas"""
+
+
+"""variable01 = 12
+variable02 = 12,3
+variabletext= 'hola mundo'
+variablelogic = True
+variablelogic = False"""
+
+
+#input se ultiliza para ingresar informacion 
+
+#Nombre = input ("Diime tu nombre: ")
+#Apellido = input ("Dime tu apellido: ")
+
