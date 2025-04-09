88d7d412df2f8ea41bd464a3633b845c84cba2c1
gonka-1 <gdrc579@gmail.com>
2025-04-09 09:31:27 -0400

    Update PR.py

--- PR.py
+++ PR.py
@@ -26,11 +26,48 @@ variablelogic = False"""
 #Nombre = input ("Diime tu nombre: ")
 #Apellido = input ("Dime tu apellido: ")
 
+#Suma
+
 N1 = int(input('Dame un numero entero:'))
 N2 = float(input('Dame un numero decimal:'))
 
-#operacion
+#operacion_1
 
 R1 = N1 + N2
 
 print (f'El resultado final es:', {R1})
+
+#Resta 
+
+N3 = int(input('Dame un numero entero:'))
+N4 = int(input('Dame un segundo numero entero:'))
+
+#Operacion_2
+
+R2 = N3 - N4
+
+print (f'El resultado de la resta es:', {R2})
+
+# Multiplicacion
+
+N5 = int(input('Dame un numero entero:'))
+N6 = int(input('dame un segundo numero entero:'))
+
+#Operacion_3
+
+R3 = N5*N6
+
+print('El resultado de la multiplicacion es:', {R3})
+
+#Division
+
+N7 = float(input('Dame un numero dececimal:'))
+N8 = float(input('Dame un segundo numero decimal:'))
+
+#Operacion_4
+
+R4 = N7 / N8 
+
+print(f'El resultado de la division es:', {R4})
+
+print('Felicidades completaste todas las operaciones!!!')
